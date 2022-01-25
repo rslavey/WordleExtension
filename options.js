f48000a0363b6f0cec99a0d84f545334200932d5
Randy Slavey <randy@randyslavey.com>
2022-01-25 14:23:51 -0700

    send options in eventlistener

--- options.js
+++ options.js
@@ -1,9 +1,11 @@
 function save_options() {
     var showTotals = document.getElementById('showtotals').checked;
     var consoleWords = document.getElementById('consolewords').checked;
+    var hideGuess = document.getElementById('hideguess').checked;
     chrome.storage.sync.set({
         showTotals: showTotals,
-        consoleWords: consoleWords
+        consoleWords: consoleWords,
+        hideGuess: hideGuess
     }, function () {
         var status = document.getElementById('status');
         status.textContent = 'Options saved.';
@@ -16,10 +18,12 @@ function save_options() {
 function restore_options() {
     chrome.storage.sync.get({
         showTotals: false,
-        consoleWords: false
+        consoleWords: false,
+        hideGuess: false
     }, function (items) {
         document.getElementById('showtotals').checked = items.showTotals;
         document.getElementById('consolewords').checked = items.consoleWords;
+        document.getElementById('hideguess').checked = items.hideGuess;
     });
 }
 document.addEventListener('DOMContentLoaded', restore_options);
